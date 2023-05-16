40f3295fa4174c2ed68f19ff98d87d234fbc80f3
zhangdy <zhangdy@cadg.cn>
2023-05-16 11:19:40 +0800

    nginx

--- documents/docker-compose/example/app.cmd
+++ documents/docker-compose/example/app.cmd
@@ -1,4 +1,4 @@
-docker-compose remove app
+docker rmi -f app:v1.0.0
 docker-compose -f docker-compose.yml down
-docker-compose build -f Dockerfile -t app:v1.0.0 .
+docker build -f Dockerfile -t app:v1.0.0 .
 docker-compose -f docker-compose.yml up -d
