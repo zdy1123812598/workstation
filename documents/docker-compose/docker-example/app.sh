2311326f6041c0fd3e20caef90787278c269edbd
zhangdy <zhangdy@cadg.cn>
2023-05-18 11:17:25 +0800

    docker-compose

--- documents/docker-compose/docker-example/app.sh
+++ documents/docker-compose/docker-example/app.sh
@@ -1,30 +1,14 @@
-#!/bin/
+#!/bin/bash
 #定义镜像和容器的名称
-name=app
-version=latest
-#判断是否容器存在
-docker ps -a | grep $name &> /dev/null
-#如果存在，关闭并删除该容器
-if [ $? -eq 0 ]
-then
-    echo $name" is up,we will stop and remove it!!!"
-    docker stop $name 
-    docker-compose -f docker-compose.yml down
-    docker rm $name 
-else
-    echo $name" is not up!!!"
-fi
 
-#判断是否镜像存在
-docker images | grep $name &> /dev/null
-#如果存在，删除该镜像
-if [ $? -eq 0 ]
-then
-    echo $name" image is existed,we will remove it!!!"
-    docker rmi $(docker images | grep $name | awk "{print $3}")
-else
-    echo $name" image is not existed!!!"
-fi
+#读取变量
+name=`cat .env | awk -F "=" 'NR==1{print $2}'  `
+version=`cat .env | awk -F "=" 'NR==2{print $2}'  `
+echo $name
+echo $version
+
+#清理
+./clean.sh
 
 #镜像生成
 docker build -f Dockerfile -t $name:$version .
